0121915efff33bb4233af64af5fdd92b55f44cc7
Hammed Oyedele <itz.harmid@gmail.com>
2020-10-10 07:28:47 +0100

    fix: disable overriding builtin functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -374,12 +374,12 @@ func evalIdentifier(
 	node *ast.Identifier,
 	env *object.Environment,
 ) object.Object {
-	if val, ok := env.Get(node.Value); ok {
-		return val
-	}
-
 	if builtin, ok := builtins[node.Value]; ok {
 		return builtin
+	}
+
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
 
 	return newError("identifier not found: " + node.Value)
