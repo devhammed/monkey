d27d140a694076e30047f18ed38d01d353102d77
Hammed Oyedele <itz.harmid@gmail.com>
2026-08-07 11:47:36 +0100

    feat: add `puts` built-in function to `io_builtins.go` for printing values

---
+++ evaluator/io_builtins.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"fmt"
+	"monkey/object"
+	"monkey/typing"
+)
+
+func init() {
+	builtins["puts"] = &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if err := typing.Check(
+				"puts",
+				args,
+				typing.MinimumArgs(1),
+			); err != nil {
+				return newError(err.Error())
+			}
+
+			for _, arg := range args {
+				fmt.Print(arg.Inspect())
+			}
+
+			fmt.Println("")
+
+			return NULL
+		},
+	}
+}
